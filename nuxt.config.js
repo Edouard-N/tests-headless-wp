bd092203083738f81341f5ee8be1eb30d34c1806
Ed <a>
2022-02-07 14:49:02 +0100

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ let dynamicRoutes = () => {
 
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: 'server',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
